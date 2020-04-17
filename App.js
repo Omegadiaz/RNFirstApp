2a7eb2f52f39ccc4eaf08394834184b20a2bdb3d
Omega Diaz <omsystem.gabriel@gmail.com>
2020-04-17 11:38:02 +0000

    Delete Items
    Added delete functionality

--- App.js
+++ App.js
@@ -1,72 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  useEffect(() => {
-   
-  });
-
-  const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
-  const [count, setCount] = useState(0);
 
-  const goalInputHandler = enteredText => {
-    
-    setEnteredGoal(enteredText);
-  };
-
-  const addGoalHandler = () => {
+  const addGoalHandler = goalTitle => {
     setCourseGoals(currentGoals => [
       ...currentGoals,
-      {key: Math.random().toString(), value: enteredGoal }
-      ]);
-    };
+      { id: Math.random().toString(), value: goalTitle }
+    ]);
+  };
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter( (goal) => goal.id !== goalId );
+    });
+  }
 
   return (
-    
-    
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Course Goal"
-          style={styles.input}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        <Button title="ADD" onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <FlatList
+        keyExtractor={(item, index) => item.id}
         data={courseGoals}
-        renderItem={itemData => (
-            <View style={styles.listItem}>
-              <Text>{itemData.item.value}</Text>
-            </View>
-        )}
-        />
+        renderItem={itemData => <GoalItem 
+          onDelete={removeGoalHandler }
+          id={itemData.item.id}
+          title={itemData.item.value} />}
+      />
     </View>
-    );
+  );
 }
 
 const styles = StyleSheet.create({
   screen: {
     padding: 50
   },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  input: {
-    width: '80%',
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 10
-  },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1
-  }
-})
+});
